7baa9d0bb0d5fdc9d8a1d3384db04157d7bac13d
Mehdieuros <nwo92@hotmail.fr>
2017-03-25 18:45:43 +0100

    Update essaiCAS.php

--- essaiCAS.php
+++ essaiCAS.php
@@ -23,21 +23,3 @@ if (isset($_REQUEST['logout'])) {
  	// for this test, simply print that the authentication was successfull
 ?>
 
-<html>
-<head>
-<title>phpCAS simple client</title>
-
-</head>
-<body>
-<h1>Successfull Authentication!</h1>
-<dl style='border: 1px dotted; padding: 5px;'>
-      <dt>Current script</dt><dd><?php print basename($_SERVER['SCRIPT_NAME']); ?></dd>
-      <dt>session_name():</dt><dd> <?php print session_name(); ?></dd>
-      <dt>session_id():</dt><dd> <?php print session_id(); ?></dd>
-</dl>
-<p>the user's login is <b><?php echo phpCAS::getUser(); ?></b>.</p>
-<p>phpCAS version is <b><?php echo phpCAS::getVersion(); ?></b>.</p>
-
-<p><a href="?logout=">Logout</a></p>
-</body>
-</html>
