177a43c4f85af07ec859638641cef3ac40111020
Mehdieuros <nwo92@hotmail.fr>
2017-03-28 17:51:27 +0200

    testFormulaire
    testFormulaire

---
+++ testFormulaire.php
@@ -0,0 +1,391 @@
+<html><head></head><body>﻿
+
+
+
+
+  
+
+    <meta charset="utf-8">
+
+    <title>Demande de subventions</title>
+
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Demande de subvention pour publication</title>
+        <link rel="stylesheet" href="Demande.css">
+       <!-- <link href='http://fonts.googleapis.com/css?family=Nunito:400,300' rel='stylesheet' type='text/css'>-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="normalize.js"></script>
+    
+
+  
+
+   
+
+
+
+	<div class="bordereau">
+		<img id="logo" src="logoparis13.png" alt="Logo iut" height="75" width="170" align="right">
+		
+		<ul id="menu">
+        <li><a href="accueil.php">Accueil</a></li>
+        <li><a href="TableaudeBord.php">Tableau de bord </a> </li>
+         
+              <li><a href="#">Demande de subvention</a>
+                <ul> 
+                        <li><a href="Demande.php"> Informations </a> </li>
+                        <li><a href="Demande_subvention1.php"> Demande pour actes de colloques</a></li>
+                        <li><a href="Demande_subvention2.php"> Demande de manifestation scientifique </a> </li>
+                        
+                </ul> </li>
+                        <li><a href="https://www.univ-paris13.fr/bred/">En savoir plus</a></li>
+		</ul>
+       		 <p> Demande|<a href="?logout=">Déconnexion</a> </p>
+					
+
+
+		<hr width="150px" align="left"> 
+	
+		<p id="pgeX">Identifiant utilisateur : 11402470 </p>
+
+		
+	
+	</div> <!-- Fin div bordereau !-->
+   
+
+
+		<hr id="grdebarre" width="1020px">
+
+		<div id="ID1">
+			<form action="index.html" method="post">
+				<h1>Formulaire de demande de subvention pour publication d’actes de colloque ou d’ouvrages thematiques collectifs</h1>
+				
+				<h4><strong>Remplir les informations obligatoire avec *</strong></h4>
+				
+				<fieldset>
+					<legend><span class="number">1</span>Coordinateur Paris 13</legend>
+						<div class="masquable ferme">
+							<h2 class="masquable-titre"><span>+</span></h2>
+							<div class="masquable-contenu" style="display: none;">
+								
+								<p><label> Prénom : </label> <input name="Composante" id="Composante" type="text"></p>
+									
+								<p><label> Nom : </label><input name="Laboratoire" id="Laboratoire" type="text"></p>
+									
+								<p><label> Qualité : </label><input name="Laboratoire" id="Laboratoire" type="text"></p>
+
+								<p><label> Composante : </label> <input name="Composante" id="Composante" type="text"></p>
+
+								<p><label> Laboratoire : </label><input name="Laboratoire" id="Laboratoire" type="text"></p>
+
+								<p><label> Type (actes, ouvrage) et nom de la publication : </label> <input name="Type" id="Type" type="text"></p>
+
+								<p><label> Date prévue et éditeur : </label> <input name="date_prevue" id="date_prevu" type="text"></p>
+
+								<p><label> Financement demandé (en €) : </label> <input name="Financement" id="Financement" type="text"></p>
+								
+							</div>
+						</div>
+				</fieldset>
+					
+					<h4><center> DEMANDE DE SUBVENTION POUR PUBLICATION </center></h4>
+					
+				<fieldset>
+					<legend><span class="number">2</span>Demande présentée par : </legend>
+					<!--------------------->
+					<div class="masquable ferme">
+						<h2 class="masquable-titre"><span>+</span></h2>
+						<div class="masquable-contenu">
+
+							<p><label> Prénom* : </label>	<input name="prenom" id="prenom" type="text"></p>
+							
+							<p><label> Nom* : </label>		<input name="nom" id="nom" type="text"></p>
+							
+							<p><label> Qualité* : </label>	<input name="qualite" id="qualite" type="text"></p>
+							
+							<p><label> Composante* : </label>	<input name="composante2" id="composante2" type="text"></p>
+							
+							<p><label> Laboratoire* : </label>	<input name="laboratoire2" id="laboratoire2" type="text"></p>
+							
+							<p><label> Téléphone* : </label>	<input placeholder="ex:0666666666" autofocus="" required="" pattern="(\d){10}" title="ex:0666666666" name="tel" id="tel" type="text"></p>
+							
+							<p><label> Mail* : </label>		<input name="mail" id="mail" type="text"></p>
+																
+						</div>
+					</div>
+				</fieldset>
+
+				<fieldset>
+					<legend><span class="number">3</span>Description de la publication</legend>
+					<div class="masquable ferme">
+						<h2 class="masquable-titre"><span>+</span></h2>
+						<div class="masquable-contenu">
+
+							<p><label> Type (actes de colloque, ouvrage collectif…) : </label> <input name="type2" id="type2" type="text"></p>
+							
+							<p><label> Titre de l’ouvrage : </label> 	<input name="titre" id="titre" type="text"></p>
+							
+							<p><label> Sous la direction de : </label>	<input name="direction" id="direction" type="text"></p>
+							
+							<p><label> Collaboration avec une autre université ou institution : </label> <input name="collabaration" id="collaboration" type="text"></p>
+							
+							<p> Nombre des contributions (attendues) : </p>
+							
+							<p><label> dont Université Paris 13 : </label> <input name="nombre_contributions_p13" id="nombre_contributionsp13" type="text"></p>
+							
+							<p><label> France : </label> <input name="nombre_contributions_fr" id="nombre_contributions_fr" type="text"></p>
+							
+							<p><label> Etranger : </label> <input name="nombre_contributions_etranger" id="nombre_contributions_etranger" type="text"></p>
+							
+							<p><label> Doctorants : </label> <input name="nombre_contributions_doc" id="nombre_contributions_doc" type="text"></p>
+							
+							<p><label> Mode de sélection des contributions  : </label> <input name="mode_de_contribution" id="mode_de_contribution" type="text"></p>
+							
+							<p> Reconnaissance de l’éditeur dans la discipline : (Document à fournir signer par la discipline)</p>
+														
+						</div>
+					</div>
+				</fieldset>
+				
+				<fieldset>
+					<legend><span class="number">4</span>Financement</legend>
+					<!--------------------->
+					<div class="masquable ferme">
+						<h2 class="masquable-titre"><span>+</span></h2>
+						<div class="masquable-contenu">
+
+							<p><label>  Montant de la contribution financière demandée par l’éditeur : </label> <input name="montant_de_la_contribution_financieremontant_de_la_contribution_financiere" id="" type="text"></p>
+							
+							<p><label> Subvention demandée au CS : </label> <input name="subvention_demande_cs" id="subvention_demande_cs" type="text"></p>
+							
+							<p><label> Co-financement : </label> <input name="Cofinancement" id="Cofinancement" type="text"></p>
+																
+						</div>
+					</div>
+				</fieldset>
+
+				<fieldset>
+					<legend><span class="number">5</span>Si la subvention est accordée par l’université souhaitez-vous : </legend>
+					<div class="masquable ferme">
+						<h2 class="masquable-titre"><span>+</span></h2>
+						<div class="masquable-contenu">
+
+							<p><label> - un virement au laboratoire ? : </label> <input class="radio" name="virement_au_laboratoire" value="oui" type="radio"> Oui <input class="radio" name="virement_au_laboratoire" value="non" type="radio"> Non </p>
+							
+							<p><label> - une gestion financière au BRED ? : </label> <input class="radio" name="gestion_financiere_bred" value="oui" type="radio"> Oui <input class="radio" name="gestion_financiere_bred" value="non" type="radio"> Non </p>
+							
+						</div>
+					</div>
+				</fieldset>
+				
+				<p> Dans les deux cas, 2 exemplaires de l’ouvrage devront être déposés au BRED à l’issue de la publication. Dans le premier cas, une photocopie des factures sera également exigée. </p>
+				<p><label> Préciser de quel appui logistique vous disposez pour la publication et, en particulier, venant de votre laboratoire : </label></p>
+				<p><input name="logistique" id="logistique" type="text"></p>
+				
+				<br>
+				<h4><center>ARGUMENTAIRE AU SOUTIEN DE LA DEMANDE DE SUBVENTION </center></h4>
+				
+				<p>	<label><strong> Pour les actes de colloque </strong>: argumentaire de deux ou trois pages développant notamment les thématiques du colloque et les approches privilégiées pour la publication en spécifiant la liste des contributeurs et les titres des articles. (3Mo Max conseillé en PDF)</label>
+					<input name="MAX_FILE_SIZE" value="3000000" type="hidden"><input name="argument_actes" type="file">
+				</p>
+				<br>
+				<p>	<label><strong> Pour un ouvrage collectif </strong>: argumentaire de deux ou trois pages spécifiant les objectifs du projet, les méthodes et les approches privilégiées, la liste des auteurs ainsi que le titre et un bref résumé des propositions de contribution à la date de la demande. (3Mo Max conseillé en PDF)</label>
+					<input name="MAX_FILE_SIZE" value="3000000" type="hidden"><input name="argument_ouvrage" type="file">
+				</p>
+				
+				<p> <input name="envoyer" value="Enregistrer le dossier" type="submit"> </p>
+
+			</form>
+			
+			<form>
+				<div>	<p>	<label for="file">Chargez le formulaire que vous aurez rempli : </label>
+										<input id="file" name="file" type="file">
+									</p>
+				</div>
+     			</form>		
+			<form>
+				<div>	<p> Une fois que vous aurez rempli votre formulaire, appuyez sur Envoyer pour envoyer le formulaire au BRED <br>
+										<input id="boutton_envoyer" value="Envoyer le formulaire" title="Envoyer votre formulaire rempli au BRED" type="submit">
+									</p>
+				</div>				
+			</form>		
+			
+		</div>
+		
+		
+		
+			<!---------------------------------------------------------------------------------------------------------------------------->
+		
+		
+		
+<!-- Remplacer "second text par la suite du formulaire -->
+		<div id="ID2" style="display:none;"><form action="index.html" method="post">
+				<h1>Formulaire de demande de subvention pour une manifestation scientifique</h1>
+			
+				<fieldset>
+					<legend><span class="numbere">1</span>Coordinateur Paris 13</legend>
+					<!--------------------->
+					<div class="masquable">
+						<h2 class="masquable-titre"><span>-</span></h2>
+						<div class="masquable-contenu">
+
+							<p>	<label for="name">Prénom : </label>			<input size="20" id="name" name="user_name" type="text">	</p>
+							 
+							<p>	<label for="mail">Nom : </label>			<input id="mail" name="user_email" type="email">			</p>
+							  
+							<p>	<label for="password">Qualité : </label>	<input id="password" name="user_password" type="password">	</p>
+							  
+							<p>	<label for="name">Composante : </label>		<input id="name" name="user_name" type="text">				</p>
+							  
+							<p>	<label for="mail">Laboratoire : </label>	<input id="mail" name="user_email" type="email">			</p>
+							  
+							<p>	<label for="password">Type (actes, ouvrage) et nom de la publication : </label>
+																			<input id="password" name="user_password" type="password">	</p>
+							<p>	<label for="name">Date prévue et éditeur : </label>
+																			<input id="name" name="user_name" type="text">				</p>
+									
+							<p>	<label for="mail">Adresse E-mail : </label>
+																			<input id="mail" name="user_email" type="email">			</p>
+
+							<p>	<label for="password">Qualité : </label>	<input id="password" name="user_password" type="password">	</p>
+									
+							<p>	<label>Age : </label>
+								<input id="under_13" value="under_13" name="user_age" type="radio">	<label for="under_13" class="light">Under 13</label>
+								<input id="over_13" value="over_13" name="user_age" type="radio">	<label for="over_13" class="light">13 or older</label>
+							</p>
+							
+						</div>
+					</div>
+				</fieldset>
+				
+				
+							<!------------------------------------------------------>
+				
+				
+				<div>
+					<legend><span class="numbere">2</span>Your profile</legend>
+					<div class="masquable ferme">
+						<h2 class="masquable-titre"><span>+</span></h2>
+						<div class="masquable-contenu">
+
+							<p>	<label for="name">Prénom : </label>			<input size="20" id="name" name="user_name" type="text">	</p>
+							 
+							<p>	<label for="mail">Nom : </label>			<input id="mail" name="user_email" type="email">			</p>
+							  
+							<p>	<label for="password">Qualité : </label>	<input id="password" name="user_password" type="password">	</p>
+							  
+							<p>	<label for="name">Composante : </label>		<input id="name" name="user_name" type="text">				</p>
+							  
+							<p>	<label for="mail">Laboratoire : </label>	<input id="mail" name="user_email" type="email">			</p>
+							  
+							<p>	<label for="password">Type (actes, ouvrage) et nom de la publication : </label>
+																			<input id="password" name="user_password" type="password">	</p>
+							<p>	<label for="name">Date prévue et éditeur : </label>
+																			<input id="name" name="user_name" type="text">				</p>
+									
+							<p>	<label for="mail">Adresse E-mail : </label>
+																			<input id="mail" name="user_email" type="email">			</p>
+
+							<p>	<label for="password">Qualité : </label>	<input id="password" name="user_password" type="password">	</p>
+									
+							<p>	<label>Age : </label>
+								<input id="under_13" value="under_13" name="user_age" type="radio">	<label for="under_13" class="light">Under 13</label>
+								<input id="over_13" value="over_13" name="user_age" type="radio">	<label for="over_13" class="light">13 or older</label>
+							</p>
+							
+						</div>
+					</div>
+
+					<legend><span class="numbere">2</span>Your profile</legend>
+					<div class="masquable ferme">
+						<h2 class="masquable-titre"><span>+</span></h2>
+						<div class="masquable-contenu">
+
+							<p>	<label for="name">Prénom : </label>			<input size="20" id="name" name="user_name" type="text">	</p>
+							 
+							<p>	<label for="mail">Nom : </label>			<input id="mail" name="user_email" type="email">			</p>
+							  
+							<p>	<label for="password">Qualité : </label>	<input id="password" name="user_password" type="password">	</p>
+							  
+							<p>	<label for="name">Composante : </label>		<input id="name" name="user_name" type="text">				</p>
+							  
+							<p>	<label for="mail">Laboratoire : </label>	<input id="mail" name="user_email" type="email">			</p>
+							  
+							<p>	<label for="password">Type (actes, ouvrage) et nom de la publication : </label>
+																			<input id="password" name="user_password" type="password">	</p>
+							<p>	<label for="name">Date prévue et éditeur : </label>
+																			<input id="name" name="user_name" type="text">				</p>
+									
+							<p>	<label for="mail">Adresse E-mail : </label>
+																			<input id="mail" name="user_email" type="email">			</p>
+
+							<p>	<label for="password">Qualité : </label>	<input id="password" name="user_password" type="password">	</p>
+									
+							<p>	<label>Age : </label>
+								<input id="under_13" value="under_13" name="user_age" type="radio">	<label for="under_13" class="light">Under 13</label>
+								<input id="over_13" value="over_13" name="user_age" type="radio">	<label for="over_13" class="light">13 or older</label>
+							</p>
+							
+						</div>
+					</div>
+				</div>
+				
+				<div class="but">
+					<button type="submit" class="but2">Sign Up</button>
+				</div>
+			</form>
+		</div>
+
+
+      
+
+
+  <!-- Footer !--> 
+
+	<hr id="grdebarre" width="1020px">
+
+	
+
+	<footer>
+
+				<ul id="menu-footer">
+
+					<a href="Contact.html" title="Contact">Contact</a>  &nbsp; &nbsp;
+
+					<a href="" title="Mentions légales">Mentions légales</a>  &nbsp; &nbsp; 
+
+					<a href="" title="Connexion">Connexion</a>
+
+					<div id="liensfooter">	
+
+						<a title="Facebook" href="https://www.facebook.com/pg/univparis13/about/?ref=page_internal"><img src="facebook.png" width="25%"></a>
+
+						<a title="Twitter" href="https://twitter.com/?lang=fr"><img src="twitter.png" width="25%"></a>
+
+						<a title="Youtube" href="https://www.youtube.com/?hl=fr&amp;gl=FR"><img src="youtube.png" width="25%"></a>
+
+						<a title="Google" href="https://www.google.fr/"><img src="google.png" width="25%"></a>
+
+					</div>
+
+				</ul>
+
+				
+
+	</footer>
+
+		
+
+	
+
+	
+
+	
+
+   
+
+
+
+
+</body></html>
