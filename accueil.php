332a7198411ea24637e328233068754097ccca5b
Amine-mrc <amine.azlouk@laposte.net>
2017-03-30 23:39:16 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -42,7 +42,8 @@ require('essaiCAS.php');
 		<hr width=150px align=left > 
 		<p id="pgeX">Identifiant utilisateur : <?php echo phpCAS::getUser(); ?> </p>
 		<hr width=150px align=left > 
-
+		<p> n° de la demande : <?php echo $_SESSION['num_publi']; ?> </p>
+		
 		<hr id = "grdebarre" width=1080px >
 		
 		
