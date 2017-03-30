4014353d8d5f8509338bd5fb26c514d3de065e72
Amine-mrc <amine.azlouk@laposte.net>
2017-03-30 23:48:08 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -42,7 +42,13 @@ require('essaiCAS.php');
 		<hr width=150px align=left > 
 		<p id="pgeX">Identifiant utilisateur : <?php echo phpCAS::getUser(); ?> </p>
 		<hr width=150px align=left > 
-		<p> n° de la demande : <?php echo SELECT num_publi from financement ?> </p>
+		<p> n° de la demande : <?php 
+			
+			$req = $bd->prepare('SELECT num_publi from financement');
+			$req->execute();
+			$resultat = $req->fetch(PDE::FETCH_ASSOC);
+			echo $resultat;
+		?> </p>
 		
 		<hr id = "grdebarre" width=1080px >
 		
