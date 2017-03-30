3d9d5f6935ac50d820e595a0bd4ec82607057ba0
Zoubair95 <zoubair.haja@gmail.com>
2017-03-30 17:29:09 +0200

    Update accueil.js

--- accueil.js
+++ accueil.js
@@ -1,12 +1,13 @@
+console.log("JS chargé")
 $(document).ready(function(){
 	
 	$('#menu li').click(function(){
 		if($(this).hasClass("active")){
-			console.log("ok");
+			console.log("a classe active");
 		}
 		else{
-			console.log("fonctionne");
-			$(this).addClass("active");
+			console.log("n'a pas la classe");
+			$(this).addClass("ajoute active");
 		}
 	});
 	
