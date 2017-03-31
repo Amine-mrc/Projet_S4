a23862f398150bb86bd14e3ae6c1ac59429759fd
AssanDiomande <assanivory@gmail.com>
2017-03-31 08:33:34 +0200

    Update Demande_subvention2.php

--- Demande_subvention2.php
+++ Demande_subvention2.php
@@ -89,7 +89,7 @@ require('essaiCAS.php');
 			{
 				$req->execute();
 				$tab = $req->fetch(PDO::FETCH_NUM);
-				echo "n° de la demande: ".(int)$tab[0]+1;
+				echo "n° de la demande: ".((int)$tab[0]+1);
 			}
 			catch(PDOException $e)
 			{
