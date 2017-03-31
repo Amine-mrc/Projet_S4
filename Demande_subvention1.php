fd3377d4767822680ce394a360fb02f1d967eff3
AssanDiomande <assanivory@gmail.com>
2017-03-31 03:20:03 +0200

    Update Demande_subvention1.php

--- Demande_subvention1.php
+++ Demande_subvention1.php
@@ -80,7 +80,7 @@ require('essaiCAS.php');
 			}
 		
 		
-			$sql='select MAX(id_publication) from publication';
+			$sql='select nom_publication from publication';
 		
 			$req = $bd->prepare($sql);
 			try	
