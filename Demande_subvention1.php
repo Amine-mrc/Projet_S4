4f5679477b032c79128de9e539a9743f2320d693
Amine-mrc <amine.azlouk@laposte.net>
2017-03-31 02:09:41 +0200

    Update Demande_subvention1.php

--- Demande_subvention1.php
+++ Demande_subvention1.php
@@ -286,13 +286,7 @@ require('essaiCAS.php');
 		<p id="pgeX">Identifiant utilisateur : <?php echo phpCAS::getUser(); ?> </p>
 		<hr width=400px align=left >
 		
-		<p> n° de la demande : <?php 
-			
-			$req = $bd->prepare('SELECT max(id_publication) from publication');
-			$req->execute();
-			$resultat = $req->fetch(PDE::FETCH_ASSOC);
-			echo $resultat
-		?> </p> 
+		
 		
 		
 		<hr id = "grdebarre" width=1080px >
